36c7cacd6347b94b9fbe615649915e223971cfa0
eugenio <eperedo13@alumnos.utalca.cl>
2020-03-05 16:22:21 -0300

    poblar la base de datos

---
+++ Base de Datos/SeederBD.sql
@@ -0,0 +1,23 @@
+
+INSERT INTO Administrador (correo, nombre, clave) VALUES ('', '', '');
+
+INSERT INTO Profesor (correo, nombre, clave) VALUES ('', '', '');
+
+INSERT INTO Ayudante (correo, nombre, clave, matricula) VALUES ('', '', '', '');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
