f1a67bfcffbea3c712ecf6e8ad41447c36712d8f
Cristobal <cristobal.henriquez.g@gmail.com>
2020-03-13 16:13:39 -0300

    vistas
    home y log de profesores

---
+++ application/modules/welcome/views/logProfe.php
@@ -0,0 +1,120 @@
+<div id="logProfesor">
+  <div class="container">
+     <div class="card" id="cambio">
+                  <div class="face face1">
+                      <div class="content">
+                          <img src="https://image.flaticon.com/icons/svg/2089/2089813.svg">
+                          <h3>Cambio</h3>
+                      </div>
+                  </div>
+                  <div class="face face2">
+                      <div class="content">
+                         <h3>Cambio</h3>
+                          <p>Texto descriptivo de la wea que se va a hacer</p>
+                            <!--form method="post" action="<?=base_url()?>index.php/cambio">
+                               <button type="submit">Entrar</button-->
+                               <a href="<?=base_url()?>index.php/cambio">Entrar</a>
+                            <!--/form-->
+                      </div>
+                  </div> 
+     </div>
+     <div class="card" id="asignatura">
+                  <div class="face face1">
+                      <div  class="content">
+                          <img src="https://image.flaticon.com/icons/svg/401/401708.svg">
+                          <h3>Asignaturas</h3>
+                      </div>
+                  </div>
+                  <div class="face face2">
+                      <div class="content">
+                          <h3>Asignaturas</h3>
+                          <p>Texto descriptivo de la wea que se va a hacer</p>
+                            <!--form method="post" action="<?=base_url()?>index.php/cambio">
+                               <button type="submit">Entrar</button-->
+                               <a href="<?=base_url()?>index.php/cambio">Entrar</a>
+                            <!--/form-->                          
+                      </div>
+                  </div> 
+     </div>
+     <div class="card" id="desempeno">
+                  <div class="face face1">
+                      <div  class="content">
+                          <img src="https://image.flaticon.com/icons/svg/1740/1740461.svg">
+                          <h3>Desempeño</h3>
+                      </div>
+                  </div>
+                  <div class="face face2">
+                      <div class="content">
+                          <h3>Desempeño</h3>
+                          <p>Texto descriptivo de la wea que se va a hacer</p>
+                            <!--form method="post" action="<?=base_url()?>index.php/cambio">
+                               <button type="submit">Entrar</button-->
+                               <a href="<?=base_url()?>index.php/desempeno">Entrar</a>
+                            <!--/form-->
+                      </div>
+                  </div> 
+     </div> 
+  </div>
+</div>
+
+   <br>
+
+<div id="logProfesor">
+  <div class="container">
+     <div class="card" id="evaluacion">
+                  <div class="face face1">
+                      <div class="content">
+                          <img src="https://image.flaticon.com/icons/svg/1312/1312253.svg">
+                          <h3>Evaluacion</h3>
+                      </div>
+                  </div>
+                  <div class="face face2">
+                      <div class="content">
+                          <h3>Evaluacion</h3>
+                          <p>Texto descriptivo de la wea que se va a hacer</p>
+                            <!--form method="post" action="<?=base_url()?>index.php/cambio">
+                               <button type="submit">Entrar</button-->
+                               <a href="<?=base_url()?>index.php/evaluacion">Entrar</a>
+                            <!--/form-->                         
+                      </div>
+                  </div> 
+     </div>
+     <div class="card" id="reunion">
+                  <div class="face face1">
+                      <div class="content">
+                          <img src="https://image.flaticon.com/icons/svg/490/490357.svg">
+                          <h3>Reunion</h3>
+                      </div>
+                  </div>
+                  <div class="face face2">
+                      <div class="content">
+                          <h3>Reunion</h3>
+                          <p>Texto descriptivo de la wea que se va a hacer</p>
+                            <!--form method="post" action="<?=base_url()?>index.php/cambio">
+                               <button type="submit">Entrar</button-->
+                               <a href="<?=base_url()?>index.php/reunion">Entrar</a>
+                            <!--/form-->                         
+                      </div>
+                  </div> 
+     </div>
+     <div class="card" id="notificacion">
+                  <div class="face face1">
+                      <div class="content">
+                          <img src="https://www.flaticon.es/premium-icon/icons/svg/2073/2073093.svg">
+                          <h3>Notificacion</h3>
+                      </div>
+                  </div>
+                  <div class="face face2">
+                      <div class="content">
+                          <h3>Notificacion</h3>
+                          <p>Texto descriptivo de la wea que se va a hacer</p>
+                            <!--form method="post" action="<?=base_url()?>index.php/cambio">
+                               <button type="submit">Entrar</button-->
+                               <a href="<?=base_url()?>index.php/notificacion">Entrar</a>
+                            <!--/form-->
+                      </div>
+                  </div> 
+     </div>       
+  </div>
+</div>
+   
