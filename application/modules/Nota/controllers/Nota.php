bbf1b696d26a5e99f9cd5f42dc93a0079c426b26
Alvaro <aelguedaicc@gmail.com>
2020-03-13 23:29:49 -0300

    CRUD Notas
    Funcionando

--- application/modules/Nota/controllers/Nota.php
+++ application/modules/Nota/controllers/Nota.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Nota extends CI_Controller {
+class Nota extends MY_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,8 +18,50 @@ class Nota extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('modelo');
+	}
 	public function index()
 	{
-		$this->load->view('nota');
+		$resultados['resultados'] = $this->modelo->listarNotas();
+
+		$this->load->view('nota', $resultados);
+	}
+
+	    function crear()
+	{
+		$data['asignaturas'] = $this->modelo->cargarDatos();
+		$data2['alumnos'] = $this->modelo->cargarAlumnos();
+
+		$this->load->view('crear', $data);
+	}
+
+    function guardarCambios(){
+		$id  	= $this->input->post("id");
+		$seccion = $this->input->post("seccion");
+		$semestre = $this->input->post("semestre");
+		$anio = $this->input->post("anio");
+		$nota = $this->input->post("nota");
+
+		$this->modelo->guardarCambios($id, $nota);
+	}
+
+	function eliminarDato(){
+		$id = $this->input->post("id");
+		$this->modelo->eliminarDato($id);
+	}
+
+    public function crearNota()
+	{
+		$refInstAsignatura = $this->input->post("asignatura");
+		$refEvaluacion 	= $this->input->post("evaluacion");
+		$refAlumno 	= $this->input->post("matricula");
+		$nota	= $this->input->post("nota");
+
+
+		$this->modelo->guardarDatos($refInstAsignatura, $refEvaluacion, $refAlumno, $nota);
+		redirect(base_url('index.php/nota'));
 	}
 }
