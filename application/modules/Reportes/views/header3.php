85074604aff723b1d34867664c738b63d91cf819
Juan Ignacio <jcorderuiz@gmail.com>
2020-04-03 04:01:12 -0300

    fix header lib

--- application/modules/Reportes/views/header3.php
+++ application/modules/Reportes/views/header3.php
@@ -3,7 +3,6 @@
       	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 
 <nav class="navbar navbar-default">
   <div class="container-fluid">
