ddb900c1cf00a0d74ce09e5ec7a8c56256f5e405
Alvaro <aelguedaicc@gmail.com>
2020-04-03 04:08:31 -0300

    FIXFIX

--- application/modules/InstanciaAsignatura/views/instanciaAsignaturaDocente.php
+++ application/modules/InstanciaAsignatura/views/instanciaAsignaturaDocente.php
@@ -4,15 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/css/hoja1.css">
-	<script type="text/javascript" src="<?= base_url()?>/js/inicio.js"></script>
-	      	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
 
 	<meta charset="utf-8">
 	<title>Vista Asignaturas</title>
